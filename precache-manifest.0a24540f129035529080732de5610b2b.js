af28ff4d240a990434fa5bb4c50fad4002385e9a
Nadika Bandara <nsbandara32@gmail.com>
2019-03-04 13:25:49 -0600

    Updates

---
+++ precache-manifest.0a24540f129035529080732de5610b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ebd7f4b3ccc23ff1a95",
+    "url": "/p2pMessaging/static/js/runtime~main.9ebd7f4b.js"
+  },
+  {
+    "revision": "6cca6d715c0f5835d741",
+    "url": "/p2pMessaging/static/js/main.6cca6d71.chunk.js"
+  },
+  {
+    "revision": "e7742408708d2bc1284f",
+    "url": "/p2pMessaging/static/js/2.e7742408.chunk.js"
+  },
+  {
+    "revision": "6cca6d715c0f5835d741",
+    "url": "/p2pMessaging/static/css/main.150ba55a.chunk.css"
+  },
+  {
+    "revision": "e7742408708d2bc1284f",
+    "url": "/p2pMessaging/static/css/2.c9c8e36e.chunk.css"
+  },
+  {
+    "revision": "69235eaca72491363d5b41d12ceae9ff",
+    "url": "/p2pMessaging/index.html"
+  }
+];
